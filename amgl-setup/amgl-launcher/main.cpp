a854d315e6a5afe5991409979dbd4ab696f87dfd
rhjoerg <rhjoerg@gmail.com>
2021-05-26 07:55:48 +0200

    #7

---
+++ amgl-setup/amgl-launcher/main.cpp
@@ -0,0 +1,15 @@
+
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	return 0;
+}
