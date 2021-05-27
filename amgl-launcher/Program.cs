ba9d6a131496090dc0a3d3bbc7a6931a6b6aacbb
rhjoerg <rhjoerg@gmail.com>
2021-05-27 09:29:25 +0200

    #7

---
+++ amgl-launcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using amgl.main;
+using System;
+using System.Windows.Forms;
+
+namespace amgl
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MainForm form = new MainForm();
+
+            Application.Run(form);
+        }
+    }
+}
