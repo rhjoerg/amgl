cee08604623236155190e41c4663ed01d78f4a55
rhjoerg <rhjoerg@gmail.com>
2021-06-04 09:15:16 +0200

    #7

--- amgl-setup/amgl-launcher/Program.cs
+++ amgl-setup/amgl-launcher/Program.cs
@@ -7,9 +7,6 @@ namespace amgl
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
