125d81e069977bca37b1062ac123bee8e275bf41
rhjoerg <rhjoerg@gmail.com>
2021-06-07 17:09:04 +0200

    #7

---
+++ amgl-launcher/amgl-launcher-content/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amgl_launcher_content
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
